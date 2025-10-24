d7273ae0b59811dffcec948202ed9eb5d305858e
nuke1115 <nuclearbomb280@gmail.com>
2025-10-25 00:04:40 +0900

    create : FileIOManager 클래스 생성

---
+++ Assets/@Scripts/Manager/Core/FileIOManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.IO;
+
+namespace Manager.Core
+{
+    public static class FileIOManager
+    {
+        /*
+        있을 기능들:
+        파일이 있는지 검사
+        파일 생성/삭제
+        파일 읽기/쓰기
+        */
+    }
+}
