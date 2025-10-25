21c4759efa71dcafc7b70338c5921938298b5ac3
nuke1115 <nuclearbomb280@gmail.com>
2025-10-26 00:51:20 +0900

    feature : stringbuilder에서 사용 가능한 문자열 비교 함수 추가

--- Assets/@Scripts/Utils/StringUtils.cs
+++ Assets/@Scripts/Utils/StringUtils.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 namespace Utils
@@ -17,7 +18,7 @@ namespace Utils
             int cnt = 0;
 
             int i = 0;
-            while(i < target.Length && (Math.Abs(target.Length - i) >= substring.Length))
+            while (i < target.Length && (Math.Abs(target.Length - i) >= substring.Length))
             {
                 if (string.Compare(target, i, substring, 0, substring.Length, System.StringComparison.Ordinal) == 0)
                 {
@@ -31,6 +32,35 @@ namespace Utils
             }
 
             return cnt;
+        }
+
+        public static int GetIndexOf(this StringBuilder target, string substring , int startIdx)
+        {
+            if (string.IsNullOrEmpty(substring) || target.Length < substring.Length || startIdx < 0 || startIdx >= target.Length)
+            {
+                return -1;
+            }
+            
+            for (int i = startIdx; i < target.Length - substring.Length; i++)
+            {
+                int index = i;
+                bool compare = true;
+                for(int internalIndex = i, subIndex = 0; subIndex < substring.Length; internalIndex++, subIndex++)
+                {
+                    if(target[internalIndex] != substring[subIndex])
+                    {
+                        compare = false;
+                        break;
+                    }
+                }
+
+                if(compare)
+                {
+                    return index;
+                }
+            }
+
+            return -1;
         }
     }
 }
