64e71e3dc5dfa7d83ff421c12eab859a0cd2a17d
daeha <203188790+ntdhk67@users.noreply.github.com>
2025-11-07 21:14:19 +0900

    1107
    데스크탑 옮기는 용도 push

--- Assets/@Scripts/Manager/Core/DataManager.cs
+++ Assets/@Scripts/Manager/Core/DataManager.cs
@@ -1,5 +1,4 @@
-
-
+using System.Collections.Generic;
 using Data;
 using UnityEngine;
 using Newtonsoft.Json;
@@ -8,10 +7,24 @@ namespace Manager.Core
 {
     public class DataManager
     {
+       public Dictionary<int, TowerData> TowerDic { get; private set; } = new Dictionary<int, TowerData>();
+
+
+
+
+
+
         //타이틀 씬에서 초기화 하는 구조던데 교보재는
         //이제 데이터 클래스 만들고, 그거 dict들 저장해야지
         public void Init()
         {
+            TowerDic = LoadJson<TowerDataLoader, int, TowerData>("TowerData").MakeDict(); //<???DataLoader, int, ???Data>("Json 경로")
+
+
+
+
+
+
 
         }
         
