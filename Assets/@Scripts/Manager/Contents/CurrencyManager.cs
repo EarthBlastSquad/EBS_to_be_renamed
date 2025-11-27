5dd983ad32114aa0b0489e1b3a3eb204a1e0065c
nuke1115 <nuclearbomb280@gmail.com>
2025-11-27 23:32:50 +0900

    feature : CurrencyManager의 변수 작성

--- Assets/@Scripts/Manager/Contents/CurrencyManager.cs
+++ Assets/@Scripts/Manager/Contents/CurrencyManager.cs
@@ -1,9 +1,11 @@
+using System;
 using UnityEngine;
 
 namespace Manager.Contents
 {
     public class CurrencyManager
     {
-
+        private int _currency = 0;
+        public event Action<int,int> OnCurrencyChangedEvent;
     }
 }
