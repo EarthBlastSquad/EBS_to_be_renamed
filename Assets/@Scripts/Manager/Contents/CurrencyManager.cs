d132ca101a695b0fe92ab5b08fe92802835d3765
nuke1115 <nuclearbomb280@gmail.com>
2025-11-28 00:27:28 +0900

    feature : 재화 양이 적절한지 확인 가능한 CanAfford추가

--- Assets/@Scripts/Manager/Contents/CurrencyManager.cs
+++ Assets/@Scripts/Manager/Contents/CurrencyManager.cs
@@ -20,5 +20,18 @@ namespace Manager.Contents
         }
 
         public int GetCurrency { get { return _currency; } }
+
+        public bool CanAfford(int amountToUse)
+        {
+            if(amountToUse < 0)
+            {
+#if DEBUG
+                Debug.LogError("사용할 재화는 음수가 될 수 없습니다");
+#endif
+                return false;
+            }
+
+            return amountToUse <= _currency;
+        }
     }
 }
