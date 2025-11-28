77ccbda3e4935a327b74a746d57ed0ee958e5d1e
nuke1115 <nuclearbomb280@gmail.com>
2025-11-29 00:01:22 +0900

    feature : HPCoordinaotr 클래스 추가

---
+++ Assets/@Scripts/Coordinator/HPCoordinator.cs
@@ -0,0 +1,23 @@
+using ComponentModule;
+using System;
+using UnityEngine;
+
+namespace Coordinator
+{
+    public class HPCoordinator : MonoBehaviour
+    {
+        private HPComponentModule _hpComponentModule;
+        public event Action<int, int> OnHPChanged;
+        
+        private void Awake()
+        {
+            _hpComponentModule = GetComponent<HPComponentModule>();
+            if(_hpComponentModule is null)
+            {
+#if DEBUG
+                Debug.LogError("hpComponentModule찾지 못함");
+#endif
+            }
+        }
+    }
+}
