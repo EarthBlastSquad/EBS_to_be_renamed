65192f4870e444bdbcb49fe45d0a7c7016148408
nuke1115 <nuclearbomb280@gmail.com>
2025-11-28 23:39:05 +0900

    feature : HP값 직접 설정 기능 추가

--- Assets/@Scripts/ComponentModule/HPComponentModule.cs
+++ Assets/@Scripts/ComponentModule/HPComponentModule.cs
@@ -1,3 +1,5 @@
+using Manager.Contents;
+using System;
 using UnityEngine;
 
 namespace ComponentModule
@@ -5,5 +7,15 @@ namespace ComponentModule
     public class HPComponentModule : MonoBehaviour
     {
         private int _hp = 0;
+
+        public void RestoreHP(int hp, Type callerType)
+        {
+            if (callerType != typeof(GameManager))
+            {
+                return;
+            }
+
+            _hp = hp;
+        }
     }
 }
