055d70e85eddffc8822769c88e36324250b610cf
nuke1115 <nuclearbomb280@gmail.com>
2025-11-29 00:19:15 +0900

    modified : HPComponentModule에 maxHP도입 및 Restore함수를 Init으로 수정

--- Assets/@Scripts/ComponentModule/HPComponentModule.cs
+++ Assets/@Scripts/ComponentModule/HPComponentModule.cs
@@ -6,10 +6,17 @@ namespace ComponentModule
     public class HPComponentModule : MonoBehaviour
     {
         private int _hp = 0;
+        private int _maxHP = 0;
 
-        public void RestoreHP(int hp)
+        public void InitHP(int maxHP)
         {
-            _hp = hp;
+            _hp = maxHP; 
+            _maxHP = maxHP;
+        }
+
+        public int GetMaxHP()
+        {
+            return _maxHP; 
         }
 
         public int GetHP()
