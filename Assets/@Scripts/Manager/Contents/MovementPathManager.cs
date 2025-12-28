dcc1c8ef63a375ade007eaf2d4877e37e5cd54ab
nuke1115 <nuclearbomb280@gmail.com>
2025-12-28 22:27:53 +0900

    이동 경로 매니저 틀 추가

---
+++ Assets/@Scripts/Manager/Contents/MovementPathManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Utils.Defines;
+using System.Collections.Generic;
+using System.Collections;
+using Contents.Grid;
+using DataStructure;
+
+namespace Manager.Contents
+{
+    public class MovementPathManager : MonoBehaviour
+    {
+        //저장하는 데이터는 그 셀의 이전 셀의 좌표
+        private Vector2Int[,] _calculatedPath = new Vector2Int[(int)MapMaxCellCnt.MAX_WIDTH, (int)MapMaxCellCnt.MAX_HEIGHT];//접근할 때  x,y 형태로 접근할 것
+        private GridManager _gridManager = null;
+        private List<Vector2Int> _startPos = new List<Vector2Int>(4);
+        private bool _dirty = false;
+        private PriorityQueue _queue = new PriorityQueue();
+
+        private void Start()
+        {
+            _gridManager = GetComponent<GridManager>();
+            if(_gridManager == null)
+            {
+                Debug.LogError("그리드 매니저가 이 오브젝트에 없음");
+            }
+        }
+
+    }
+}
