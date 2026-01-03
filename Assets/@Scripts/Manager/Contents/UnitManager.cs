4178dc1cb3856b69c5c631b3e7fdf0297de23a03
nuke1115 <nuclearbomb280@gmail.com>
2026-01-03 17:53:51 +0900

    feature:UnitManager에 유닛 Act호출 제작

--- Assets/@Scripts/Manager/Contents/UnitManager.cs
+++ Assets/@Scripts/Manager/Contents/UnitManager.cs
@@ -9,9 +9,24 @@ namespace Manager.Contents
         private List<UnitCoordinator> _unitCoordinators = new List<UnitCoordinator>(128);
         private Queue<UnitCoordinator> _unitBuffer = new Queue<UnitCoordinator>(8);
 
+        
+
+
         public void AddUnit(UnitCoordinator unit)
         {
             _unitBuffer.Enqueue(unit);
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < _unitCoordinators.Count; i++)
+            {
+                if (_unitCoordinators[i].IsDead())
+                {
+                    continue;
+                }
+                _unitCoordinators[i].Act();
+            }
         }
 
         private void LateUpdate()
