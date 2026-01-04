e31d26e97ec27f7c9e628cb447168218e7a27b31
ntdhk67 <daehana06@kakao.com>
2026-01-04 21:19:31 +0900

    Reapply "PrettyUI"
    Sprites가 담긴 에셋파일의 일부를 추가하였습니다.
    해당 sprite 요소에 TestPreLoad를 붙여, Adressables의 default그룹을 수정하게 되었습니다.
    Title 및 Lobby 씬에 백그라운드 추가, Lobby 씬에 설명 팝업이 추가되었습니다.

--- Assets/@Scripts/UI/Popup/UI_InventoryPopup.cs
+++ Assets/@Scripts/UI/Popup/UI_InventoryPopup.cs
@@ -15,7 +15,9 @@ namespace UI.Popup
         enum GameObjects
         {
             Slider_0,
-            Inventorys_0
+            Inventorys_0,
+            TowerDataPopUp_0,
+            Background_0
         }
         enum Buttons
         {
@@ -78,6 +80,17 @@ namespace UI.Popup
         }
 
         #endregion
+        public void OpenTDP(Contents.Tower.Tower t)
+        {
+            GetObject((int)GameObjects.TowerDataPopUp_0).SetActive(true);
+            GetObject((int)GameObjects.Background_0).SetActive(true);
+            GetObject((int)GameObjects.TowerDataPopUp_0).GetComponent<UI_TowerDataPopup>().OpenTowerData(t);
+        }
+        public void CloseTDP()
+        {
+            GetObject((int)GameObjects.TowerDataPopUp_0).SetActive(false);
+            GetObject((int)GameObjects.Background_0).SetActive(false);
+        }
     }
 }
 
