cb95780fdf88735aea4251d423906c2d9ff769b6
nuke1115 <nuclearbomb280@gmail.com>
2026-01-12 16:25:36 +0900

    add:VictimCoordinator에 hp컴포넌트 추가

--- Assets/@Scripts/Coordinator/VictimCoordinator.cs
+++ Assets/@Scripts/Coordinator/VictimCoordinator.cs
@@ -7,15 +7,18 @@ namespace Coordinator
     public class VictimCoordinator : MonoBehaviour
     {
         private CooldownComponentModule _cooldown;
+        private HPCoordinator _hpCoordinator;
 
         private void Awake()
         {
             _cooldown = gameObject.GetOrAddComponent<CooldownComponentModule>();
+            _hpCoordinator = gameObject.GetOrAddComponent<HPCoordinator>();
         }
 
-        public void InitVictim(float invincibilityTime)
+        public void InitVictim(float invincibilityTime, int maxHP)
         {
             _cooldown.InitCooldown(invincibilityTime);
+            _hpCoordinator.InitHP(maxHP);
         }
     }
 }
