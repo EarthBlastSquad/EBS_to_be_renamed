8152c80b3e24e80fd114093f124f9e81815baaf8
nuke1115 <nuclearbomb280@gmail.com>
2026-01-12 18:56:07 +0900

    feature:AttackManager에 요청 처리 기능 추가

--- Assets/@Scripts/Manager/Contents/AttackManager.cs
+++ Assets/@Scripts/Manager/Contents/AttackManager.cs
@@ -19,5 +19,25 @@ namespace Manager.Contents
         {
             _queue.Enqueue(arg);
         }
+
+        private void LateUpdate()
+        {
+            if(Managers.Instance.GameManager.IsGamePaused)
+            {
+                return; 
+            }
+
+            while(_queue.Count > 0)
+            {
+                var request = _queue.Dequeue();
+
+                if(request.Item1 is null || request.Item2 is null)
+                {
+                    continue;
+                }
+
+                request.Item1.Act(request.Item2);//딱 공격만 하고, 체력 까임에 따른 이벤트는 각자가 알아서 처리할 것
+            }
+        }
     }
 }
