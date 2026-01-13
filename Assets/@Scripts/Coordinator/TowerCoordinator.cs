0ca23fbd97046df574c887e4f115bd18d46efb33
nuke1115 <nuclearbomb280@gmail.com>
2026-01-13 23:00:54 +0900

    feature: TowerCoordinator에 타워 공격 활성화/비활성화 함수 추가

--- Assets/@Scripts/Coordinator/TowerCoordinator.cs
+++ Assets/@Scripts/Coordinator/TowerCoordinator.cs
@@ -1,6 +1,7 @@
 using ComponentModule;
 using Data;
 using Manager;
+using Manager.Contents;
 using UnityEngine;
 using Utils;
 
@@ -13,17 +14,41 @@ namespace Coordinator
         private CooldownComponentModule _module;
         private VictimCoordinator _victimCoordinator;
         private Vector2Int _facing;
+        private GridManager _gridManager;
+        private bool _isAttackState = false;
+
+        private Vector2Int _placedPos; //이건 GridPosComponentModule로 처리하까도 생각했는데, 나중에 생각해보죠. 근데, 그건 이동시스템을 위해 만든건데, 이동시스템이 아직 없으니까 넣는건 너무 섯부른 판단일듯
 
         private void Awake()
         {
             _victimCoordinator = gameObject.GetOrAddComponent<VictimCoordinator>();
+            _gridManager = FindAnyObjectByType<GridManager>();
         }
 
-        public void Init(TowerData data, Vector2Int facing)
+        public void Init(TowerData data, Vector2Int facing, Vector2Int placedPos)
         {
             _facing = facing;
             _victimCoordinator.InitVictim(data.InvincibilityTime, data.TowerHP);
             _skillData = Managers.Instance.DataManager.SkillDic[data.SkillId];
+            _placedPos = placedPos;
+        }
+
+        public void UpdateState()
+        {
+            _isAttackState = CheckMobIsInRange();
+        }
+
+        private bool CheckMobIsInRange()
+        {
+            for(int i = 0; i < _skillData.AttackPos.Count; i++)
+            {
+                if (_gridManager.TryGetReadonlyVictimList(_placedPos + (_skillData.AttackPos[i]*_facing), out var list) && list.Count > 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
