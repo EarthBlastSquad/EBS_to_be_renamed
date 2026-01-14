416cd3a4fb39ca9e27d8482d7017432349391fa3
nuke1115 <nuclearbomb280@gmail.com>
2026-01-15 00:08:01 +0900

    feature: 공격 가능한지 여부를 비트마스킹으로 알 수 있게 수정

--- Assets/@Scripts/Coordinator/BaseSkillCoordinator.cs
+++ Assets/@Scripts/Coordinator/BaseSkillCoordinator.cs
@@ -6,10 +6,21 @@ namespace Coordinator
     public abstract class BaseSkillCoordinator : MonoBehaviour
     {
         private SkillData _skillData;
+        private int _attackableLayers;
 
         public void Init(SkillData skillData)
         {
             _skillData = skillData;
+            _attackableLayers = 0;
+            for(int i = 0; i < _skillData.AttackableLayers.Count; i++)
+            {
+                _attackableLayers |= (1<<_skillData.AttackableLayers[i]);
+            }
+        }
+
+        public bool CanAttack(int bitmaskedLayer)
+        {
+            return (_attackableLayers & bitmaskedLayer) != 0;
         }
 
         public abstract bool Act(VictimCoordinator victim);
