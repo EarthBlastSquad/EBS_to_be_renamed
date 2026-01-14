1ff66d5449841c099c911f4b517e843ff2aa6557
nuke1115 <nuclearbomb280@gmail.com>
2026-01-14 23:37:55 +0900

    feature: ProjectileCoordinator에초기화 함수 작성

--- Assets/@Scripts/Coordinator/ProjectileCoordinator.cs
+++ Assets/@Scripts/Coordinator/ProjectileCoordinator.cs
@@ -1,5 +1,7 @@
 using Actor;
 using ComponentModule;
+using Data;
+using Manager;
 using UnityEngine;
 using Utils;
 
@@ -20,6 +22,13 @@ namespace Coordinator
             Debug.LogError("스킬이 없습니다.");
 #endif
             _actor = gameObject.GetOrAddComponent<ProjectileMovementActor>();
+        }
+
+        public void Init(SkillData data, Vector2Int gridCnt, Vector3 startPos, Vector3 endPos)
+        {
+            _posModule.Init(startPos, endPos, data.SpeedPerCell*(gridCnt.x + gridCnt.y));
+            _actor.Init(Managers.Instance.ResourceManager.Load<Sprite>(data.AttackEffectName), Managers.Instance.ResourceManager.Load<Sprite>(data.AttackObjectImgName));
+            _accumulatedTime = 0;
         }
     }
 }
