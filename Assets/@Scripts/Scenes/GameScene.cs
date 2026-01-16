eaf79da7d90e712f9df8634c167909befeb7aeb5
nuke1115 <nuclearbomb280@gmail.com>
2026-01-16 18:17:34 +0900

    feature:GameScene에 웨이브 시작 코드 작성

--- Assets/@Scripts/Scenes/GameScene.cs
+++ Assets/@Scripts/Scenes/GameScene.cs
@@ -1,4 +1,6 @@
 using Data;
+using Manager;
+using Manager.Contents;
 using System;
 using UnityEngine;
 
@@ -6,11 +8,40 @@ namespace Scenes
 {
     public class GameScene : BaseScene
     {
+
         public Action<WaveData> OnWaveChanged;
+
+        private WaveManager _waveMgr;
+
+        public void OnSecondEnd()
+        {
+            if(_waveMgr.TryGetNextWave() == false)
+            {
+                return;
+            }
+            Managers.Instance.TimerManager.StartTimer(OnSecondEnd,_waveMgr.GetNowWaveData().WaveTimeLimit, 1f);
+        }
+
         protected override void Init()
         {
             base.Init();
             SceneType = Utils.Defines.SceneNames.GameScene;
+            _waveMgr = FindAnyObjectByType<WaveManager>();
+
+            bool result = _waveMgr.Init(Managers.Instance.StageManager.GetNowStageData().WaveIdx);
+
+#if UNITY_EDITOR
+            if(result == false)
+            {
+                Debug.LogError("웨이브 매니저 초기화 실패");
+            }
+#endif
+        }
+
+        private void Start()
+        {
+            Managers.Instance.TimerManager.StartTimer(OnSecondEnd, _waveMgr.GetNowWaveData().WaveTimeLimit, 1f);
+            OnWaveChanged?.Invoke(_waveMgr.GetNowWaveData());
         }
     }
 }
