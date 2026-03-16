1890380c0d71b1fc3d6ab5681a68d30e58057dd1
nuke1115 <nuclearbomb280@gmail.com>
2026-03-17 00:43:44 +0900

    fix: dotween버그 시즌2 및 취소 시 array out of index오류 나는거 수정

--- Assets/@Scripts/UI/Popup/UI_Shop.cs
+++ Assets/@Scripts/UI/Popup/UI_Shop.cs
@@ -137,7 +137,7 @@ namespace UI.Popup
         {
             Managers.Instance.SoundManager.Play(SoundChannels.EFFECT_0, "ButtonPress", false);
             ShopOpen = false;
-            _s = Slots.None;
+         
             CloseSR();
             _rp.Hide();
             SelectedSlot();
@@ -161,6 +161,8 @@ namespace UI.Popup
             _uiSelectTower.position = _selectTower.transform.parent.position + new Vector3(0, -0.25f+posY, 0);
             if (ShopOpen == false)
             {
+                _selectTower.DOKill();
+                _selectTower.color = Color.white;
                 _selectTower.transform.parent.rotation = Quaternion.identity;
                 _selectTower.DOFade(0.3f, 0.15f).SetLoops(-1, LoopType.Yoyo);
                 SelectedSlot();
